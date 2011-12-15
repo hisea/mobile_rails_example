595c59e061b0a9a31ddca777419b51a620e1f05d
Hisea <zyinghai@gmail.com>
2011-12-14 23:48:00 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileRailsExample::Application.config.secret_token = 'c91c3be07af75e3c2b5ea76b70d8a8d676046d8d131bb0f02e08f6802e1d7335edc6b28867bd57d6dd4df35d78f20d4fbf9d488b6a67bbdd2e629936aea2fa65'
